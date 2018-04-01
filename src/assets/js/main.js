763c7927e0ba653bfdcf01c2c54c565500cbdfa1
serii <seriiburduja@gmail.com>
2018-04-01 18:46:53 +0300

    change some files

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,23 +2,28 @@ $(function () {
   /*
       https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html
       ===========================*/
-  $("html").easeScroll({
-    frameRate: 60,
-    animationTime: 1000,
-    stepSize: 90,
-    pulseAlgorithm: 1,
-    pulseScale: 8,
-    pulseNormalize: 1,
-    accelerationDelta: 20,
-    accelerationMax: 1,
-    keyboardSupport: true,
-    arrowScroll: 50,
-    touchpadSupport: true,
-    fixedBackground: true
-  });
+      $("html").easeScroll({
+        frameRate: 60,
+        animationTime: 1000,
+        stepSize: 190,
+        pulseAlgorithm: 1,
+        pulseScale: 8,
+        pulseNormalize: 1,
+        accelerationDelta: 20,
+        accelerationMax: 1,
+        keyboardSupport: true,
+        arrowScroll: 50,
+        touchpadSupport: true,
+        fixedBackground: false
+      });
 
+      function(something in themes){
+        'something';
+        'another string for me';
+        this.letter = string;
+      }
 
-});
+    });
 
 
 
